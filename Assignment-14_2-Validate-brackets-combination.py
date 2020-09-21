6e37b7a57e8b8c8503c0ec3cf4ff2a5ff6a4ba0f
rmzturkmen <rmzturkmen72@gmail.com>
2020-09-21 23:05:55 +0200

    Validate-brackets-combination

---
+++ Assignment-14_2-Validate-brackets-combination.py
@@ -0,0 +1,24 @@
+# Validate-brackets-combination
+
+def check(text):
+    pairs = {')': '(', ']': '[', '}': '{'}
+    que = []
+    for i in text:
+        if i in pairs.values():
+            que.append(i)
+        elif i in pairs.keys():
+            if que[-1] == pairs[i]:
+                que.pop(-1)
+            else:
+                return False  # parantez açılmadan kapanmış
+        else:
+            return False  # geçersiz karakter
+    return False if que else True  # que de eleman varsa False
+
+
+print(check("()"))
+print(check("()[]()"))
+print(check("(]"))
+print(check("([)]"))
+print(check("{[]}"))
+print(check(""))
